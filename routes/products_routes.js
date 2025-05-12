efebfc6bd497f08295c513f80bdc4fb05c598c86
hagerMahmoud82 <hm658216@gmail.com>
2025-05-12 10:53:54 +0300

    Update products_routes.js

--- routes/products_routes.js
+++ routes/products_routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const {
   getAllProducts,
   getSingleProduct,
+  addNewProduct,
 } = require("../controllers/getAllProducts");
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
 
 router.route("/:id").get(getSingleProduct);
+
+
+router.route("/add").post(addNewProduct);
 
 module.exports = {
   router,
