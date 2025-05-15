65233456e6048f68d033f0c621d7644901025067
hagerMahmoud82 <hm658216@gmail.com>
2025-05-15 18:03:36 +0300

    Update getAllProducts.js

--- controllers/getAllProducts.js
+++ controllers/getAllProducts.js
@@ -1,7 +1,7 @@
 const Product = require("../modules/productsSchema.js");
 const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find();  
+        const products = await Product.find({});  
         return res.status(200).json(products);  
       } catch (error) {
         return res.status(500).json({ message: 'Error retrieving products', error: error.message });
